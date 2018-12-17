e07604bf757c81ec2067900e62ac95f4374901c4
Fernando Herrera <fernando@ciedigital.com>
2018-12-17 13:18:14 -0800

    Added redux-form and create a form.

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
+  renderInput({input, label}) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input}/>
+      </div>
+    )
+  }
+
   render () {
-    return <div>StreamCreate</div>
+    return (
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter Title" type="text" />
+        <Field name="description" component={this.renderInput} label="Enter Description" type="text" />
+      </form>
+    );   
   }
 }
 
-export default StreamCreate;
+export default reduxForm({
+  form: 'streamCreate'
+})(StreamCreate);
