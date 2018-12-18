2ba2067c011a4963e9e39eb8b72d1d2f85875f39
Fernando Herrera <fernando@ciedigital.com>
2018-12-18 10:47:57 -0800

    API for streams created

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,25 +2,66 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-  renderInput({input, label}) {
+  renderError({error, touched}) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = ({
+    input,
+    label,
+    meta
+  }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;    
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
-        <input {...input}/>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
       </div>
     )
   }
 
+  onSubmit(formProps) {
+    console.log(formProps)
+  }
+
   render () {
     return (
-      <form className="ui form">
+      <form 
+        onSubmit={this.props.handleSubmit(this.onSubmit)} 
+        className="ui form error"
+        >
         <Field name="title" component={this.renderInput} label="Enter Title" type="text" />
         <Field name="description" component={this.renderInput} label="Enter Description" type="text" />
+        <button className="ui button primary">Submit</button>
       </form>
     );   
   }
 }
 
+const validate = (formProps) => {
+  const errors = {};
+
+  if (!formProps.title) {
+    errors.title = 'Required'
+  } else if (formProps.title.length > 15) {
+    errors.title = 'Must be 15 characters or less'
+  }
+
+  if (!formProps.description) {
+    errors.description = 'Required'
+  }
+
+  return errors;
+}
+
 export default reduxForm({
-  form: 'streamCreate'
+  form: 'streamCreate',
+  validate
 })(StreamCreate);
