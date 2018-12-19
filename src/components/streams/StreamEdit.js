385e33e20758c3b8629dd78838584ca761780d47
Fernando Herrera <fernando@ciedigital.com>
2018-12-18 18:08:03 -0800

    Loaded stream object to edit page

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
 class StreamEdit extends React.Component {
-  render () {
-    return <div>StreamEdit</div>
+  
+  componentDidMount() {
+    const streamId = this.props.match.params.id;
+    this.props.fetchStream(streamId);
   }
+
+  renderEdit = stream => {
+    if (!stream) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <h3>{stream.title}</h3>
+        <p>{stream.description}</p>
+      </div>
+    )
+  }
+
+  render () {
+    return (
+      <div>
+        <h2>Stream Edit</h2>
+        {this.renderEdit(this.props.stream)}
+      </div>
+    );
+  };
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
