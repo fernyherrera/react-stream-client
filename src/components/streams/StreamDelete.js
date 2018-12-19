d4500e9ed8d925755a2d2ac53166c5fb983696cb
Fernando Herrera <fernando@ciedigital.com>
2018-12-19 14:16:41 -0800

    Init modal

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Modal from '../Modal';
 
 class StreamDelete extends React.Component {
   render () {
-    return <div>StreamDelete</div>
+    return (
+      <div>
+        <h3>Stream Delete</h3>
+        <Modal></Modal>
+      </div>
+    );
   }
 }
 
