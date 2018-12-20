7bd540338d4dc18a95988aeed745e30093136f8f
Fernando Herrera <fernando@ciedigital.com>
2018-12-20 11:42:12 -0800

    Added props to modal component.

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
 
 class StreamDelete extends React.Component {
+  actions = (
+    <React.Fragment>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>
+  );
+
   render () {
     return (
       <div>
         <h3>Stream Delete</h3>
-        <Modal></Modal>
+        <Modal
+          title="Delete Stream"
+          content="Are you sure you want to delete this string?"
+          actions={this.actions}
+          onDismiss={() => history.push('/')}
+        ></Modal>
       </div>
     );
   }
